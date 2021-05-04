668a82ab067c8d13a14c3c899428aefdbb51d91b
Gautam Botrel <gautam.botrel@gmail.com>
2021-05-04 15:04:55 -0500

    feat: added bls24_315 in apicheck_test.go

--- internal/apicheck_test.go
+++ internal/apicheck_test.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	bls377 "github.com/consensys/gnark-crypto/ecc/bls12-377"
-	bls381 "github.com/consensys/gnark-crypto/ecc/bls12-381"
+	bls12377 "github.com/consensys/gnark-crypto/ecc/bls12-377"
+	bls12381 "github.com/consensys/gnark-crypto/ecc/bls12-381"
+	bls24315 "github.com/consensys/gnark-crypto/ecc/bls24-315"
 	"github.com/consensys/gnark-crypto/ecc/bn254"
-	bw761 "github.com/consensys/gnark-crypto/ecc/bw6-761"
+	bw6761 "github.com/consensys/gnark-crypto/ecc/bw6-761"
 )
 
 // note: pairing API is not code generated, and don't use interfaces{} for performance reasons
@@ -13,28 +14,32 @@ import (
 var err error
 
 var (
-	gtbls377 bls377.GT
-	gtbls381 bls381.GT
-	gtbn254  bn254.GT
-	gtbw761  bw761.GT
+	gtbls12_377 bls12377.GT
+	gtbls12_381 bls12381.GT
+	gtbn254     bn254.GT
+	gtbw6_761   bw6761.GT
+	gtbls24_315 bls24315.GT
 )
 
 func init() {
 	// Pair
-	gtbls377, err = bls377.Pair([]bls377.G1Affine{}, []bls377.G2Affine{})
-	gtbls381, err = bls381.Pair([]bls381.G1Affine{}, []bls381.G2Affine{})
+	gtbls12_377, err = bls12377.Pair([]bls12377.G1Affine{}, []bls12377.G2Affine{})
+	gtbls12_381, err = bls12381.Pair([]bls12381.G1Affine{}, []bls12381.G2Affine{})
 	gtbn254, err = bn254.Pair([]bn254.G1Affine{}, []bn254.G2Affine{})
-	gtbw761, err = bw761.Pair([]bw761.G1Affine{}, []bw761.G2Affine{})
+	gtbw6_761, err = bw6761.Pair([]bw6761.G1Affine{}, []bw6761.G2Affine{})
+	gtbls24_315, err = bls24315.Pair([]bls24315.G1Affine{}, []bls24315.G2Affine{})
 
 	// MillerLoop
-	gtbls377, err = bls377.MillerLoop([]bls377.G1Affine{}, []bls377.G2Affine{})
-	gtbls381, err = bls381.MillerLoop([]bls381.G1Affine{}, []bls381.G2Affine{})
+	gtbls12_377, err = bls12377.MillerLoop([]bls12377.G1Affine{}, []bls12377.G2Affine{})
+	gtbls12_381, err = bls12381.MillerLoop([]bls12381.G1Affine{}, []bls12381.G2Affine{})
 	gtbn254, err = bn254.MillerLoop([]bn254.G1Affine{}, []bn254.G2Affine{})
-	gtbw761, err = bw761.MillerLoop([]bw761.G1Affine{}, []bw761.G2Affine{})
+	gtbw6_761, err = bw6761.MillerLoop([]bw6761.G1Affine{}, []bw6761.G2Affine{})
+	gtbls24_315, err = bls24315.MillerLoop([]bls24315.G1Affine{}, []bls24315.G2Affine{})
 
 	// FinalExp
-	gtbls377 = bls377.FinalExponentiation(&gtbls377)
-	gtbls381 = bls381.FinalExponentiation(&gtbls381)
+	gtbls12_377 = bls12377.FinalExponentiation(&gtbls12_377)
+	gtbls12_381 = bls12381.FinalExponentiation(&gtbls12_381)
 	gtbn254 = bn254.FinalExponentiation(&gtbn254)
-	gtbw761 = bw761.FinalExponentiation(&gtbw761)
+	gtbw6_761 = bw6761.FinalExponentiation(&gtbw6_761)
+	gtbls24_315 = bls24315.FinalExponentiation(&gtbls24_315)
 }
